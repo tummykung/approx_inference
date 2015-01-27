6bbe687b6245571c28294c632ac5fb0065b0e5dc
Tum Chaturapruek <sorathan@cs.stanford.edu>
2015-01-27 03:09:06 -0800

    Add fixed eta experiment

--- Rotation/src/toy.py
+++ Rotation/src/toy.py
@@ -7,30 +7,43 @@ import random
 import simplejson
 import numpy as np
 
-k = 3
-n = 10
-m = 9
+k = 3 # sentence length
+n = 4 # range X
+
+range_Y = int(np.ceil(np.log(n)/np.log(2))) + 1
+m = [range_Y, range_Y, 9]
 
 learning_verbose = False
 prediction_verbose = False
+sanity_check = False
 true_theta = np.array([0.0, 0.0, 0.0, 0.0]) # to be initialized
 X = range(1, n + 1)
-Y = range(m + 1)
+Y0 = range(m[0] + 1)
+Y1 = range(m[1] + 1)
+Y2 = range(m[2] + 1)
+W = range(3)
 Z = {}
 probability = {}
 num_samples_per_x = 15
 samples = []
 samples_by_x = {}
+eta = None # eta = None means we use a non-relaxed model
 
 step_size = 0.1
 
 def main():
-    if(len(sys.argv) != 5):
-        print "must have exactly 4 arguments for the 4 values of theta"
+    eta = None # this is needed.
+    if(len(sys.argv) < 5 or len(sys.argv) > 6):
+        print "(current number of argumets = {num}".format(num=len(sys.argv) - 1)
+        print "Must have at least 4 arguments for the 4 values of theta."
+        print "If the fifth value is specified, that will be eta, and we'll use a relaxed model with"
+        print "the corresponding eta"
         sys.exit(2)
     else:
         for j in range(4):
             true_theta[j] = float(sys.argv[j + 1])
+        if (len(sys.argv) == 6):
+            eta = float(sys.argv[5])
     generate_data()
 
     for element in samples:
@@ -49,18 +62,27 @@ def main():
     best_theta = train(train_data)
 
     print
+    if eta is not None:
+        print "eta = " + str(eta)
+    else:
+        print "no relaxation"
+
+    print "rangeY = " + str(m)
+    print "vocab size(X) = " + str(n)
+
     print "best_theta = " + str(best_theta)
     print "true_theta = " + str(true_theta)
     diff = best_theta - true_theta
     norm_diff = np.linalg.norm(diff)
     print "L2 difference = " + str(norm_diff)
+    print
 
     (
         best_expert_count_correct,
         best_expert_total_count,
         best_expert_accuracy,
         best_expert_total_log_likelihood
-    ) = get_accuracy(true_theta, test_data)
+    ) = get_accuracy(true_theta, test_data, eta)
     print "best_expert_accuracy = {accuracy} ({count}/{total})".format(
         accuracy=best_expert_accuracy,
         count=best_expert_count_correct,
@@ -74,7 +96,7 @@ def main():
         total_count,
         accuracy,
         total_log_likelihood
-    ) = get_accuracy(best_theta, test_data)
+    ) = get_accuracy(best_theta, test_data, eta)
     print "accuracy = {accuracy} ({count}/{total})".format(
         accuracy=accuracy,
         count=count_correct,
@@ -83,25 +105,25 @@ def main():
     print "average log_likelihood = {t}".format(t=total_log_likelihood/float(total_count))
 
 
-def get_accuracy(theta, test_data):
+def get_accuracy(theta, test_data, eta):
     print "predicting and getting accuracy..."
     count = 0
     count_correct = 0
     total_log_likelihood = 0.0
     for sample in test_data:
-        y_correct = sample[0]
+        w_correct = sample[0]
         x = sample[1]
-        (y, log_likelihood) = predict(x, theta)
+        (w, log_likelihood) = predict(x, theta, eta)
         total_log_likelihood += log_likelihood
         correct = False
-        if(y == y_correct):
+        if(w == w_correct):
             correct = True
             count_correct += 1
         if(prediction_verbose):
             print "x:{x}, expected: {expected}, predicted: {predicted}, correct?: {correct}".format(
                 x=x,
-                expected=y_correct,
-                predicted=y,
+                expected=w_correct,
+                predicted=w,
                 correct=correct
             )
         count += 1
@@ -109,31 +131,43 @@ def get_accuracy(theta, test_data):
         return (count_correct, count, None, total_log_likelihood)
     return (count_correct, count, float(count_correct)/count, total_log_likelihood)
 
-def predict(x, theta):
+def predict(x, theta, eta):
     maxValue = -1.0
-    maxY = (0, 0, 0)
-    for y in list(itertools.product(Y, Y, Y)):
-        value = p(y, x, theta)
+    maxW = (0, 0, 0)
+    for w in itertools.product(W, W, W):
+        value = real_p(w, x, theta, eta)
         if (value > maxValue):
             maxValue = value
-            maxY = y
-    return (maxY, maxValue)
+            maxW = w
+
+    return (maxW, maxValue)
 
 def train(train_data):
     theta_hat = np.array([0.0, 0.0, 0.0, 0.0])
     theta_hat_average = np.array([0.0, 0.0, 0.0, 0.0])
-    total = np.array([0.0, 0.0, 0.0, 0.0])
     counter = 0
     for sample in train_data:
         counter += 1
         if(learning_verbose):
             print counter
-        y = sample[0]
+        w = sample[0]
         x = sample[1]
-        gradient = gradient_phi(y, x)
-        Z = calculate_Z(x, theta_hat_average)
-        E_grad_phi = expectation_gradient_phi(x, theta_hat, Z)
-        new_grad = step_size * (gradient - E_grad_phi)
+        new_grad = np.array([0.0, 0.0, 0.0, 0.0])
+        for y in itertools.product(Y0, Y1, Y2):
+            y = np.array(y)
+            gradient = gradient_phi(y, x)
+            Z = calculate_Z(x, theta_hat_average)
+            E_grad_phi = expectation_gradient_phi(x, theta_hat, Z)
+            grad_log_Y = step_size * (gradient - E_grad_phi)
+
+            if eta is None:
+                the_q_component = q(w, y)
+            else:
+                the_q_component = q_relaxed(w, y, eta)
+            new_grad += grad_log_Y * p(y, x, theta_hat) * the_q_component
+
+        new_grad /= real_p(w, x, theta_hat, eta)
+
         theta_hat += new_grad
         # total += theta_hat
         # theta_hat_average = total / counter
@@ -159,7 +193,7 @@ def expectation_gradient_phi(x, theta, Z):
         product = 1.0
         for j in range(3):
             if j != i:
-                product *= (m + math.exp(theta[j]))
+                product *= (m[j] + math.exp(theta[j]))
             else:
                 product *= math.exp(theta[j])
         the_sum += product / Z
@@ -170,7 +204,7 @@ def expectation_gradient_phi(x, theta, Z):
 
     total[3] = float(1)/Z * math.exp(theta[3]) * common_factor
     # Before this it was slow because I iterated through all Y^3
-    # for y in list(itertools.product(Y, Y, Y)):
+    # for y in list(itertools.product(Y0, Y1, Y2)):
     #     gradient = gradient_phi(y, x)
     #     total += p(y, x, theta) * gradient
     return total
@@ -193,18 +227,20 @@ def gradient_phi(y, x):
 
 def generate_data():
     print "generating data..."
-    for x in list(itertools.product(X, X, X)):
+    for x in itertools.product(X, X, X):
+        # can't convert to tuple since can't hash array
         probability[x] = {}
         Z[x] = calculate_Z(x, true_theta)
-        for y in list(itertools.product(Y, Y, Y)):
-            probability[x][y] = p(y, x, true_theta)
+        for w in list(itertools.product(W, W, W)):
+            probability[x][w] = real_p(w, x, true_theta, None) # important to not generate from eta
 
         # sanity check
-        # print sum(probability[x].values())
+        if sanity_check:
+            print "sanity check... " + str((x, sum(probability[x].values())))
 
         for i in range(num_samples_per_x):
-            y = sample(x)
-            samples.append((y, x))
+            w = sample(x)
+            samples.append((w, x))
     print "done generating data. Total of {size} samples".format(size=len(samples))
     # print simplejson.dumps(samples)
 
@@ -234,7 +270,7 @@ def calculate_Z(x, theta):
     total = 0.0
     product = 1.0
     for i in range(3):
-        product *= (m + math.exp(theta[i]))
+        product *= (m[i] + math.exp(theta[i]))
     total += product
 
     exponent = 0.0
@@ -243,6 +279,41 @@ def calculate_Z(x, theta):
             exponent += theta[i]
     total += (math.exp(theta[3]) - 1) * math.exp(exponent)
     return total
+
+def real_p(w, x, theta, eta):
+    total = 0.0
+    for y in itertools.product(Y0, Y1, Y2):
+        y = np.array(y)
+        if eta is None:
+            q_factor = q(w, y)
+        else:
+            q_factor = q_relaxed(w, y, eta)
+        total += p(y, x, theta) * q_factor
+    return total
+
+def q_relaxed(w, y, eta):
+    return np.exp(-eta * hamming(w, denotation(y)))/np.power(1.0 + 2.0 * np.exp(-eta), 3.0)
+
+def hamming(x, y):
+    """x and y must have the same length, else it will cut off the tail of the longer one."""
+    total = 0
+    min_length = np.min([len(x), len(y)])
+    for i in range(min_length):
+        total += np.abs(x[i] - y[i])
+    return total
+
+def denotation(y):
+    return y % 3 # assuming y is np.array, so this operator should work element-wise
+
+def q(w, y):
+    output = True
+    for j in range(3):
+        if (y[j] % 3 != w[j]):
+            output = False
+    if output:
+        return 1.0
+    else:
+        return 0.0
 
 def p(y, x, theta):
     exponent = 0.0
